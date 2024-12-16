4ec47cffe8afd5faf794bbefc686c66e78e0bef0
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2024-12-16 19:44:44 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config();
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}));
+export {app};
