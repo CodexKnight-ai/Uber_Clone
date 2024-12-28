ad11ce0e771cd913fe0166ea8377ebbf738a25d2
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2024-12-28 19:12:25 +0530

    User and captain auth complete

--- Frontend/src/store/authSlice.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from "@reduxjs/toolkit";
-// import captainReducer from "./captainSlice.js"
-// import userReducer from "./userSlice.js"
-
-// const authReducer=combineReducers({
-//     user:userReducer
-// })
